ddb36785ab5b388953c733dc2f8f3d0f60dd7084
fujulia <juliafu713@gmail.com>
2023-11-28 17:05:55 -0300

    feito

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let button = document.querySelector(".button");
 let operadores = document.querySelector(".gray");
 let input = document.querySelector("#display");
 let history = document.querySelector("#history");
+let btnIgual = document.getElementById("btnIgual");
 let operar = '';
 let raiz;
 
@@ -12,43 +13,39 @@ function add_numero(numero) {
     if (operar == '') {
         input.value += numero;
     }
+    btnIgual.focus();
 }
 
 function add_operador(operador) {
-    if (operador == "+") { operar = "+" };
-    if (operador == "-") { operar = "-" };
-    if (operador == "*") { operar = "*" };
-    if (operador == "/") { operar = "/" };
-    if (operador == "^") { operar = "**" };
+    operar = operador;
     input.value += operar
-    console.log(input.value);
 }
 
-document.addEventListener('keypress', function (event) {
-    if (event.key === 'Enter') {
-        console.log(input.value);
+document.addEventListener('keydown', function (event) {
+    console.log(event.key)
+    if (!isNaN(event.key) || event.key == "/" || event.key == "*" || event.key == "+" || event.key == "-") {
+        input.value = input.value + event.key;
+    }
+    if(event.key == "="|| event.key == "Enter"){
         total();
+    }
+    if(event.key == "Backspace"|| event.key == "Delete" || event.key == "Escape"){
+        limpar();
     }
 });
 
 function total() {
     let guardar = input.value;
     history.innerHTML = guardar;
-    input.value = eval(guardar);
-    input.value = raiz;
-    console.log((eval(guardar)));
-
+    input.value = eval(guardar)
 }
-
 
 function limpar() {
     input.value = '';
-
 }
 
 function calcularRaiz(){
-    input.toString;
+    input.toString();
     raiz = Math.sqrt(input.value);
-
 }
 
