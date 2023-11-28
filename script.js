90604f8a937c79acc7ba75cd88abdfe22aece888
fujulia <juliafu713@gmail.com>
2023-11-28 05:41:11 -0300

    feito

--- script.js
+++ script.js
@@ -3,34 +3,52 @@ let operadores = document.querySelector(".gray");
 let input = document.querySelector("#display");
 let history = document.querySelector("#history");
 let operar = '';
+let raiz;
 
-
-function add_numero (numero) {
-    if(operar =! ''){
+function add_numero(numero) {
+    if (operar = ! '') {
         input.value += numero;
     }
-    if(operar == ''){
-        input.value+=numero;
+    if (operar == '') {
+        input.value += numero;
     }
 }
 
-function add_operador(operador){
-    if(operador == "+"){ operar = "+"};
-    if(operador == "-"){operar = "-"};
-    if(operador == "*"){operar = "*"};
-    if(operador == "/"){operar = "/"};
-    input.value+=operador
+function add_operador(operador) {
+    if (operador == "+") { operar = "+" };
+    if (operador == "-") { operar = "-" };
+    if (operador == "*") { operar = "*" };
+    if (operador == "/") { operar = "/" };
+    if (operador == "^") { operar = "**" };
+    input.value += operar
     console.log(input.value);
-  
 }
 
-function total(){
+document.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        console.log(input.value);
+        total();
+    }
+});
+
+function total() {
     let guardar = input.value;
     history.innerHTML = guardar;
-    guardar.toString();
     input.value = eval(guardar);
+    input.value = raiz;
     console.log((eval(guardar)));
-  
+
 }
 
+
+function limpar() {
+    input.value = '';
+
+}
+
+function calcularRaiz(){
+    input.toString;
+    raiz = Math.sqrt(input.value);
+
+}
 
